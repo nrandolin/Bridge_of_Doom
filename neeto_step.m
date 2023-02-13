6120b186a4992aecd63c9dc55ada6a60661541d9
Daniel Sudzilouski <dsudzilouski@olin.edu>
2023-02-13 15:35:00 -0500

    clean up the binary search

--- neeto_step.m
+++ neeto_step.m
@@ -1,31 +1,26 @@
 
 function [next_u, V_l_command, V_r_command, V_vec, dT_hat_dt] = neeto_step(current_u, delta_t, V_symbolic, dT_hat_dt_symbolic, V_l_symbolic, V_r_symbolic, sub_eval_symb, DU_DT_SCAN, DU_DT_SCAN_MAX, MAX_NEETO_SPEED)
     optimal_du_dt = 0;
-    V_l_command = 0;
-    V_r_command = 0;
+    V_l_command = 1000000;
+    V_r_command = 1000000;
     
     du_min = DU_DT_SCAN;
     du_max = DU_DT_SCAN_MAX;
-    while(abs(du_min - du_max) > 0.001)
-        du_mid = (du_min + du_max) / 2;
-        V_l_discrete = sub_eval_symb(V_l_symbolic, current_u, du_mid);
-        V_r_discrete = sub_eval_symb(V_r_symbolic, current_u, du_mid);
-        if(abs(V_l_discrete) < MAX_NEETO_SPEED && abs(V_r_discrete) < MAX_NEETO_SPEED)
-            du_min = du_mid;
+       
+
+    while((abs(V_l_command) < MAX_NEETO_SPEED && abs(V_r_command) < MAX_NEETO_SPEED) == 0 || abs(du_max - du_min) > 0.001)
+        optimal_du_dt = (du_min + du_max) / 2;
+        V_l_command = sub_eval_symb(V_l_symbolic, current_u, optimal_du_dt);
+        V_r_command = sub_eval_symb(V_r_symbolic, current_u, optimal_du_dt);
+        if(abs(V_l_command) < MAX_NEETO_SPEED && abs(V_r_command) < MAX_NEETO_SPEED)
+            du_min = optimal_du_dt;
         else
-            du_max = du_mid;
+            du_max = optimal_du_dt;
         end
-
-
     end
    
-    optimal_du_dt = du_mid;
-    V_l_command = sub_eval_symb(V_l_symbolic, current_u, du_mid);
-    V_r_command = sub_eval_symb(V_r_symbolic, current_u, du_mid);
-    V_vec = sub_eval_symb(V_symbolic, current_u, du_mid);
-    dT_hat_dt = sub_eval_symb(dT_hat_dt_symbolic, current_u, du_mid);
-
-
+    V_vec = sub_eval_symb(V_symbolic, current_u, optimal_du_dt);
+    dT_hat_dt = sub_eval_symb(dT_hat_dt_symbolic, current_u, optimal_du_dt);
     next_u = current_u + delta_t * optimal_du_dt
 end
 
